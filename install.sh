9985da863bd6354be9d50bc67f6b279678fe6e38
Alex Parker <misterparker@gmail.com>
2014-05-19 19:45:06 -0600

    Re-arrange order of things and whitespace

--- install.sh
+++ install.sh
@@ -10,33 +10,34 @@ sudo apt-get -y install \
 
 sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
 
-git clone https://github.com/alexparker/dotfiles ~/.dotfiles
+# RUBY
+  # Install Rbenv
+    git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 
-echo "export ENV_LABEL='Remote Dev'" >> ~/.bashrc
-echo "export ENV_COLOR=$(tput setaf 160)" >> ~/.bashrc
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
-# Install fresh
-bash -c "`curl -sL https://raw.githubusercontent.com/alexparker/fresh/master/install.sh`"
-
-echo "source ~/.fresh/build/shell.sh" >> ~/.bashrc
-
-# Install spf13 fork
-sh <(curl https://raw.githubusercontent.com/alexparker/spf13-vim/3.0/bootstrap.sh)
-
-# Install Rbenv
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-
-# Install Ruby-build plugin for Rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+    # Install Ruby-build plugin for Rbenv
+    git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 # Install github CLI wrapper for git
+  git clone https://github.com/github/hub.git /tmp/hub
+  cd /tmp/hub
+  rake install prefix=/usr/local
 
-git clone https://github.com/github/hub.git /tmp/hub
-cd /tmp/hub
-rake install prefix=/usr/local
+# Install dotfiles
+  git clone https://github.com/alexparker/dotfiles ~/.dotfiles
+
+  echo "export ENV_LABEL='Remote Dev'" >> ~/.bashrc
+  echo "export ENV_COLOR=$(tput setaf 160)" >> ~/.bashrc
+
+# Install fresh
+  bash -c "`curl -sL https://raw.githubusercontent.com/alexparker/fresh/master/install.sh`"
+  echo "source ~/.fresh/build/shell.sh" >> ~/.bashrc
+
+# Install spf13 fork
+  sh <(curl https://raw.githubusercontent.com/alexparker/spf13-vim/3.0/bootstrap.sh)
+
 
 
 # TODO:
