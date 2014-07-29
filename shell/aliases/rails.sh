2aaebfcedf615d9659774c43190a6a96766f3a16
Alex Parker <alex@bigcartel.com>
2014-07-29 16:43:18 -0600

    Fix typo in rails aliases

--- shell/aliases/rails.sh
+++ shell/aliases/rails.sh
@@ -2,7 +2,7 @@ alias spr="spring rails"
 alias runspec='spring rspec `find . -type f -name "*_spec.rb" | selecta`'
 
 alias brake="bundle exec rake"
-alias briails="bundle exec rails"
+alias brails="bundle exec rails"
 
 alias rollback="bundle exec rake db:rollback"
 alias migrate="bundle exec rake db:migrate"
