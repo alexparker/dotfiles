a41472899f518564ef107785cf6579e9aed3ad91
Alex Parker <github@alexparker.me>
2017-10-16 13:59:01 -0600

    All sorts of new goodies

---
+++ plugins/hammerspoon/init.lua
@@ -0,0 +1,40 @@
+hs.window.animationDuration = 0 -- disable animations
+
+hs.hotkey.bind({"shift", "alt"}, "F", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"shift", "alt"}, "R", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"shift", "alt"}, "V", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
