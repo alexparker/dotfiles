4acee378e9610f6a6e1d8a4bf719fae6ec8b33b3
Alex Parker <github@alexparker.me>
2017-11-09 08:46:37 -0700

    Add some hammerspoon shortcuts

--- plugins/hammerspoon/init.lua
+++ plugins/hammerspoon/init.lua
@@ -1,5 +1,6 @@
 hs.window.animationDuration = 0 -- disable animations
 
+-- Height: 100%, Width: 50%, LEFT
 hs.hotkey.bind({"shift", "alt"}, "F", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -13,6 +14,7 @@ hs.hotkey.bind({"shift", "alt"}, "F", function()
   win:setFrame(f)
 end)
 
+-- Height: 100%, Width: 50%, RIGHT
 hs.hotkey.bind({"shift", "alt"}, "R", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -26,6 +28,35 @@ hs.hotkey.bind({"shift", "alt"}, "R", function()
   win:setFrame(f)
 end)
 
+-- Height: 50%, Width: 100%, BOTTOM
+hs.hotkey.bind({"shift", "alt"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- Height: 50%, Width: 100%, TOP
+hs.hotkey.bind({"shift", "alt"}, "K", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+-- 100% X 100%
 hs.hotkey.bind({"shift", "alt"}, "V", function()
   local win = hs.window.focusedWindow()
   local f = win:frame()
@@ -37,4 +68,17 @@ hs.hotkey.bind({"shift", "alt"}, "V", function()
   f.w = max.w
   f.h = max.h
   win:setFrame(f)
+end)
+
+k = hs.hotkey.modal.new({"shift", "alt"}, 'O') 
+k:bind('', 'escape', function() k:exit() end)
+k:bind('', 'H', function() 
+	local win = hs.window.focusedWindow()
+	win:moveOneScreenWest()
+	k:exit()
+end)
+k:bind('', 'L', function() 
+	local win = hs.window.focusedWindow()
+	win:moveOneScreenEast()
+	k:exit()
 end)
